c1ae7ec22879d2f1b6abd17bc533fcfbf0db4fef
howard3917 <71679856+howard3917@users.noreply.github.com>
2021-05-11 15:57:46 +0800

    program_to be countinue

---
+++ KG_class/Assets/scripts/Bouncing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bouncing : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Vector3 force;
+
+
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+         
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(Collision.gameObject.tag == "floor")
+        {
+            print("kiss");
+        rb.AddForce(force);
+    }
+    }
+}
